693f28b64c910cdb64b7ee861f5e72d3b8114a89
suprisemnisi <140027867+suprisemnisi@users.noreply.github.com>
2023-07-21 01:43:58 +0200

    Add files via upload

---
+++ GreetGUIApp.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package greetguiapp;
+
+import za.ac.tut.ui.GreetGUI;
+
+/**
+ *
+ * @author khens
+ */
+public class GreetGUIApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        GreetGUI frame1;
+//        frame1 =
+            new GreetGUI();
+    }
+    
+}
